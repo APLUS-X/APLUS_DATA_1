67a6a05c93602e93138dabf2675fca027ce6abe5
APLUS-X <56814982+APLUS-X@users.noreply.github.com>
2019-12-02 17:33:35 +0800

    Add files via upload

--- Code/jiandan_1.py
+++ Code/jiandan_1.py
@@ -3,11 +3,27 @@
 
 import requests
 from bs4 import BeautifulSoup
+import random
+
+
+def get_ua():
+    first_num = random.randint(55, 78)
+    third_num = random.randint(0, 3200)
+    fourth_num = random.randint(0, 140)
+    os_type = [
+        '(Windows NT 6.1; WOW64)', '(Windows NT 10.0; WOW64)', '(X11; Linux x86_64)',
+        '(Macintosh; Intel Mac OS X 10_12_6)'
+    ]
+    chrome_version = f'Chrome/{first_num}.0.{third_num}.{fourth_num}'
+    ua = ' '.join(['Mozilla/5.0', random.choice(os_type), 'AppleWebKit/537.36',
+                   '(KHTML, like Gecko)', chrome_version, 'Safari/537.36']
+                  )
+    return ua
 
 
 index = 0
 headers = {'referer': 'http://jandan.net/',
-           'user-agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64; rv:47.0) Gecko/20100101 Firefox/47.0'}
+           'user-agent': get_ua()}
 
 
 # 保存图片
