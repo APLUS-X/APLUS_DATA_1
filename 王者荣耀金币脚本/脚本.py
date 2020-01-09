a6c8d53a787b886ba46ab8851e3f39a25cb0f825
APLUS-X <56814982+APLUS-X@users.noreply.github.com>
2020-01-09 16:45:28 +0800

    Update 脚本.py

--- 王者荣耀金币脚本/脚本.py
+++ 王者荣耀金币脚本/脚本.py
@@ -10,21 +10,25 @@ print('刷金脚本运行···')
 def aplus(x, y):
     os.system('adb shell input tap {} {}'.format(x, y))
 
+
+def a_plus():
+    aplus(500, 500)  # 点击屏幕继续
+    print("点击继续")
+    sleep(2)
+    aplus(1800, 990)  # 再次挑战
+    print("点击再次",end='->')
+    sleep(2)
+    aplus(1660, 900)  # 闯关加载
+    print("点击闯关",end='->')
+    sleep(15)
+    aplus(1900, 10)  # 跳过
+    print('点击跳过',end='->')
+    sleep(2)
+    aplus(1800, 50)  # 自动
+    print("点击自动",end='->')
+    sleep(200)
+    print(f'结束····以执行{i + 1}次')
+    a_plus()
+
 if __name__ == '__main__':
-    for i in range(repeat_num):
-        if(i > 0):
-            aplus(1880, 990)  # 再次挑战
-            print("再次")
-            sleep(2)
-        aplus(1660, 900)#闯关加载
-        print("闯关")
-        sleep(18)
-        aplus(1800,50)#跳过
-        sleep(2)
-        aplus(1800, 50)#自动
-        print("点击自动")
-        sleep(190)
-        aplus(500,500)#点击屏幕继续
-        print("测试点击")
-        sleep(2)
-        print(f'执行次数{i+1}')
+    a_plus()
